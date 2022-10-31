89bcf914b43558ff26a7be243001be3a32ed06b4
wuyajun.0 <wuyajun.0@bytedance.com>
2022-10-31 14:49:20 +0800

    init
    Change-Id: Id3d4fe40064e365f8f23aacf0c762a27b1ffd0fe

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/yajw/atw-downloader
+
+go 1.18
+
+require github.com/avast/retry-go v3.0.0+incompatible
+
+require (
+	github.com/anaskhan96/soup v1.2.5 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+	golang.org/x/text v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/anaskhan96/soup"
+	"github.com/avast/retry-go"
+)
+
+const (
+	src = "https://www.tingclass.net/list-7751-%d.html"
+)
+
+type Show struct {
+	siteURL   string
+	Title     string
+	SeqNumber string
+}
+
+func main() {
+	shows := make(chan *Show, 1000)
+	go func() {
+		for pageNumber := 1; pageNumber <= 1; pageNumber++ {
+			listURL := fmt.Sprintf(src, pageNumber)
+			err := retry.Do(func() error {
+				resp, err := soup.Get(listURL)
+				if err != nil {
+					return err
+				}
+				doc := soup.HTMLParse(resp)
+				children := doc.FindAll("li", "class", "clearfix")
+
+				/**
+				<li class="clearfix">
+				   <em class="fr">浏览：676</em>
+				   <span class="fl class_num">第461篇:</span>
+					<a class="ell" href="https://www.tingclass.net/show-7751-334158-1.html" target="_blank" title="Eating in Hongkong">Eating in Hongkong</a>
+				</li>
+				*/
+
+				for i := range children {
+					child := children[i]
+					span := child.Find("span", "class", "class_num")
+					a := child.Find("a", "class", "ell")
+					seqNumber := strings.TrimLeft(strings.TrimRight(span.Text(), "篇:"), "第")
+					seq, err := strconv.ParseInt(seqNumber, 10, 64)
+					if err == nil {
+						seqNumber = fmt.Sprintf("%04d", seq)
+					}
+					shows <- &Show{
+						siteURL:   a.Attrs()["href"],
+						SeqNumber: seqNumber,
+						Title:     a.Attrs()["title"],
+					}
+				}
+				return nil
+			})
+
+			if err != nil {
+				println(fmt.Sprintf("url=%s,err=%v", listURL, err))
+				break
+			}
+		}
+		close(shows)
+	}()
+
+	wg := &sync.WaitGroup{}
+	for show := range shows {
+		go func(s *Show) {
+			wg.Add(1)
+
+			defer wg.Done()
+			err := retry.Do(func() error {
+				resp, err := soup.Get(s.siteURL)
+				if err != nil {
+					return err
+				}
+				doc := soup.HTMLParse(resp)
+				children := doc.FindAll("div", "id", "mp3")
+				for i := range children {
+					downloadURL := children[i].Text()
+					prefix, postfix := urlFileName(downloadURL)
+					fileName := buildFilename(fmt.Sprintf("%v.%s %v.%s", s.SeqNumber, prefix, s.Title, postfix))
+					println(fmt.Sprintf("%v,url=%s", fileName, downloadURL))
+				}
+				return nil
+			})
+			if err != nil {
+				println(fmt.Sprintf("url=%v,err:=%v", s.siteURL, err))
+			}
+		}(show)
+	}
+	wg.Wait()
+}
+
+func buildFilename(s string) string {
+	return strings.ReplaceAll(
+		strings.ReplaceAll(
+			strings.ReplaceAll(
+				strings.ReplaceAll(
+					strip(s), "  ", "",
+				), " \" ", "\""),
+			"  ", ""),
+		"？", "")
+}
+
+func strip(s string) string {
+	var result strings.Builder
+	for i := 0; i < len(s); i++ {
+		b := s[i]
+		if b != '"' {
+			result.WriteByte(b)
+		}
+	}
+	return result.String()
+}
+func downloadFile(filepath string, url string) (err error) {
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check server response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	// Writer the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func stringify(o interface{}) string {
+	bts, err := json.Marshal(o)
+	if err != nil {
+		return fmt.Sprintf("<err:%v>", err)
+	}
+	return string(bts)
+}
+
+func urlFileName(url string) (string, string) {
+	r, _ := http.NewRequest("GET", url, nil)
+	parts := strings.Split(path.Base(r.URL.Path), ".")
+
+	return "20" + strings.Join(parts[0:len(parts)-1], ""), parts[len(parts)-1]
+}
